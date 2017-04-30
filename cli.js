271322b02eb41f9ffd45a1c53ff4d29155f0d9d8
Gabriel Mangiurea <mangiurea.gabriel@gmail.com>
2017-04-30 18:32:30 +0300

    Refactor: XO

--- cli.js
+++ cli.js
@@ -2,141 +2,141 @@
 
 'use strict';
 
-const brkn         = require('brkn'),
-      chalk        = require('chalk'),
-      yargs        = require('yargs'),
-      isWebUri     = require('valid-url').isWebUri,
-      eventEmitter = require('brkn/event_emitter');
+const brkn = require('brkn');
+const chalk = require('chalk');
+const yargs = require('yargs');
+const isWebUri = require('valid-url').isWebUri;
+const eventEmitter = require('brkn/event-emitter');
 
-const util         = require('./util'),
-      packageJSON  = require('./package.json');
+const util = require('./util');
+const packageJSON = require('./package.json');
 
 const argv = yargs
-  .command({
-    command: ['[url|file...]', '*'],
-    desc: false,
-    handler: argv => {
-      let sources = argv._;
+	.command({
+		command: ['[url|file...]', '*'],
+		desc: false,
+		handler: argv => {
+			const sources = argv._;
 
-      if(!argv.verbose) {
-        util.startTimer();
-      }
+			if (!argv.verbose) {
+				util.startTimer();
+			}
 
-      if(isWebUri(sources[0])) {
-        try {
-          brkn(sources, argv.attr, util.parseBaseUrl(sources[0]), {verbose: argv.verbose});
-        } catch(e) {
-          setImmediate(() => {
-            eventEmitter.emit('error', e.message);
-          });
-        }
-      } else {
-        try {
-          brkn(sources, argv.attr, argv.base, {verbose: argv.verbose});
-        } catch(e) {
-          setImmediate(() => {
-            eventEmitter.emit('error', e.message);
-          });
-        }
-      }
-    }
-  })
-  .option('attr', {
-    alias: 'a',
-    default: ['href', 'src'],
-    describe: 'The attributes to search for (space separated if more than one)',
-    type: 'array'
-  })
-  .option('base', {
-    alias: 'b',
-    describe: 'The base URL (needed to resolve relative URLs; files only)',
-    type: 'string'
-  })
-  .option('verbose', {
-    alias: 'v',
-    describe: 'Run in verbose mode',
-    type: 'boolean'
-  })
-  .usage('Usage: \x0A\u00A0\u00A0$ brkn <url|file...> [--attr [html attribute...]] [--base <hostname (w/ protocol)>] [--verbose]')
-  .example('$ brkn https://httpbin.org')
-  .example('$ brkn ./fixtures/page1.html ./fixtures/page2.html --base "https://mywebsite.com"')
-  .example('$ brkn ./fixtures/*.html --base "https://mywebsite.com" --verbose')
-  .help('help', 'Show this screen')
-  .epilog('MIT \u00A9 Gabriel Mangiurea')
-  .version(packageJSON.version)
-  .argv;
+			if (isWebUri(sources[0])) {
+				try {
+					brkn(sources, argv.attr, util.parseBaseUrl(sources[0]), {verbose: argv.verbose});
+				} catch (err) {
+					setImmediate(() => {
+						eventEmitter.emit('error', err.message);
+					});
+				}
+			} else {
+				try {
+					brkn(sources, argv.attr, argv.base, {verbose: argv.verbose});
+				} catch (err) {
+					setImmediate(() => {
+						eventEmitter.emit('error', err.message);
+					});
+				}
+			}
+		}
+	})
+	.option('attr', {
+		alias: 'a',
+		default: ['href', 'src'],
+		describe: 'The attributes to search for (space separated if more than one)',
+		type: 'array'
+	})
+	.option('base', {
+		alias: 'b',
+		describe: 'The base URL (needed to resolve relative URLs; files only)',
+		type: 'string'
+	})
+	.option('verbose', {
+		alias: 'v',
+		describe: 'Run in verbose mode',
+		type: 'boolean'
+	})
+	.usage('Usage: \x0A\u00A0\u00A0$ brkn <url|file...> [--attr [html attribute...]] [--base <hostname (w/ protocol)>] [--verbose]')
+	.example('$ brkn https://httpbin.org')
+	.example('$ brkn ./fixtures/page1.html ./fixtures/page2.html --base "https://mywebsite.com"')
+	.example('$ brkn ./fixtures/*.html --base "https://mywebsite.com" --verbose')
+	.help('help', 'Show this screen')
+	.epilog('MIT \u00A9 Gabriel Mangiurea')
+	.version(packageJSON.version)
+	.argv;
 
 eventEmitter.on('error', error => {
-  util.stopTimer();
+	util.stopTimer();
 
-  if(error.payload) {
-    util.printError(error.payload)
-  } else {
-    util.printError(error);
-  }
+	if (error.payload) {
+		util.printError(error.payload);
+	} else {
+		util.printError(error);
+	}
 });
 
 eventEmitter.on('item', item => {
-  if(item.broken) {
-    util.consoleLog(
-      chalk.cyan.bold('Scanned URL:'),
-      chalk.red(item.url),
-      chalk.red(item.statusCode),
-      item.source ? `[${item.source}]` : ''
-    );
-  } else {
-    util.consoleLog(
-      chalk.cyan('Scanned URL:'),
-      item.url,
-      chalk.green(item.statusCode),
-      item.source ? `[${item.source}]` : ''
-    );
-  }
+	if (item.broken) {
+		util.consoleLog(
+			chalk.cyan.bold('Scanned URL:'),
+			chalk.red(item.url),
+			chalk.red(item.statusCode),
+			item.source ? `[${item.source}]` : ''
+		);
+	} else {
+		util.consoleLog(
+			chalk.cyan('Scanned URL:'),
+			item.url,
+			chalk.green(item.statusCode),
+			item.source ? `[${item.source}]` : ''
+		);
+	}
 });
 
 eventEmitter.on('source', source => {
-  const brokenUrls = source.brokenUrls;
+	const brokenUrls = source.brokenUrls;
 
-  if(brokenUrls.length) {
-    util.consoleLog(
-      chalk.yellow(
-        'Found',
-        chalk.bold(brokenUrls.length),
-        `broken link${brokenUrls.length > 1 ? 's' : ''} in ${chalk.bold(source.source)}`
-      )
-    )
-  }
+	if (brokenUrls.length > 0) {
+		util.consoleLog(
+			chalk.yellow(
+				'Found',
+				chalk.bold(brokenUrls.length),
+				`broken link${brokenUrls.length > 1 ? 's' : ''} in ${chalk.bold(source.source)}`
+			)
+		);
+	}
 });
 
 eventEmitter.on('end', brokenUrls => {
-  if(util.timerId) {
-    util.stopTimer();
-  }
+	if (util.timerId) {
+		util.stopTimer();
+	}
 
-  util.consoleLog(
-    chalk.cyan.bold('Scanning completed!')
-  );
+	util.consoleLog(
+		chalk.cyan.bold('Scanning completed!')
+	);
 
-  if(brokenUrls.length) {
-    util.consoleLog(
-      'The following',
-      chalk.bold(brokenUrls.length),
-      `URL${brokenUrls.length > 1 ? 's were' : ' was'} broken or could not accept GET requests:`
-    );
+	if (brokenUrls.length > 0) {
+		util.consoleLog(
+			'The following',
+			chalk.bold(brokenUrls.length),
+			`URL${brokenUrls.length > 1 ? 's were' : ' was'} broken or could not accept GET requests:`
+		);
 
-    for(let idx in brokenUrls) {
-      util.consoleLog(
-        `${~~idx + 1}:`,
-        `${brokenUrls[idx]}`
-      );
-    }
+		for (let i = 0, l = brokenUrls.length; i < l; i++) {
+			util.consoleLog(
+				`${~~i + 1}:`,
+				`${brokenUrls[i]}`
+			);
+		}
 
-    if(!argv.verbose) {
-      util.consoleLog(
-        `${chalk.cyan('Hint:')} use ${chalk.bold('--verbose (-v)')} for additional details during the scan`
-      );
-    }
-  } else {
-    util.consoleLog('There were no broken URLs.')
-  }
+		if (!argv.verbose) {
+			util.consoleLog(
+				`${chalk.cyan('Hint:')} use ${chalk.bold('--verbose (-v)')} for additional details during the scan`
+			);
+		}
+	} else {
+		util.consoleLog('There were no broken URLs.');
+	}
 });
--- util.js
+++ util.js
@@ -1,47 +1,47 @@
 'use strict';
 
-const chalk            = require('chalk'),
-      urllib           = require('url'),
-      loadingIndicator = require('loading-indicator'),
-      loadingPresets   = require('loading-indicator/presets');
+const urllib = require('url');
+const chalk = require('chalk');
+const loadingIndicator = require('loading-indicator');
+const loadingPresets = require('loading-indicator/presets');
 
-const packageJSON  = require('./package.json');
+const packageJSON = require('./package.json');
 
 const util = {
-  timerId: null, // placeholder for the loading indicator
+	timerId: null,
 
-  startTimer: () => {
-    util.timerId = loadingIndicator.start('Scanning...', {
-      frames: loadingPresets.dots
-    });
-  },
+	startTimer: () => {
+		util.timerId = loadingIndicator.start('Scanning...', {
+			frames: loadingPresets.dots
+		});
+	},
 
-  stopTimer: timerId => {
-    loadingIndicator.stop(util.timerId);
-    util.timerId = null;
-  },
+	stopTimer: () => {
+		loadingIndicator.stop(util.timerId);
+		util.timerId = null;
+	},
 
-  parseBaseUrl: urlString => {
-    let url = urllib.parse(urlString);
-    return `${url.protocol}${url.slashes ? '//' : null}${url.hostname}`
-  },
+	parseBaseUrl: urlString => {
+		const url = urllib.parse(urlString);
+		return `${url.protocol}${url.slashes ? '//' : null}${url.hostname}`;
+	},
 
-  consoleLog: (...args) => {
-    // prefix every console.log message with the package name
-    console.log.apply(console, [
-      chalk.bgBlack(`${packageJSON.name}`),
-      ...args
-    ]);
-  },
+	consoleLog: (...args) => {
+		// Prefix every console.log message with the package name
+		console.log(
+			chalk.bgBlack(`${packageJSON.name}`),
+			...args
+		);
+	},
 
-  printError: error => {
-    util.consoleLog(
-      chalk.bgRed('ERR'),
-      chalk.red(error)
-    );
+	printError: error => {
+		util.consoleLog(
+			chalk.bgRed('ERR'),
+			chalk.red(error)
+		);
 
-    return false;
-  }
-}
+		return false;
+	}
+};
 
 module.exports = util;
